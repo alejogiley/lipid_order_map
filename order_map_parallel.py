98cd582e6775c781ff8be24e504b261a43f6d91c
alejogiley <alejogiley@gmail.com>
2018-12-07 13:23:55 -0500

    order_map_parallel.py

--- order_map_parallel.py
+++ order_map_parallel.py
@@ -7,11 +7,22 @@
 #
 #	P2 = 0.5 * (3 * <cos^2(theta)> - 1)
 #
+# where theta is the angle between the bond and the bilayer normal: 
+#
+# P2 = 1 means perfect alignment with the bilayer normal
+# P2 = -0.5 anti-alignment
+# P2 = 0 random orientation.
+#
 # The average vale along all acyl chain bonds
 # is stored in a 2D grid, where the bin coordinates are assigned
 # according to the (x,y) position of the lipid PO4 bead.  
 # Finally, the order-parameter matrix is average over time. 
 # ****the membrane normal vector is assumed to be (0,0,1)
+#
+# hardcode variables:
+#
+#	stride = 1
+#	gridsize = 1
 #
 # 2018 Alejandro Gil-Ley, NHLBI/NIH
 #
@@ -24,10 +35,11 @@ import sys
 import math
 import time
 import signal
-import optparse
+import argparse
 import numpy as np
 import MDAnalysis as mda
 import multiprocessing as mp
+from scipy import interpolate
 from MDAnalysis.analysis.leaflet import LeafletFinder
 
 ##################################################################################################################################################################
@@ -39,28 +51,25 @@ def parse_cmdline(cmdlineArgs):
 	This function initializes the command line parser.
 	"""
 	
-	parser = optparse.OptionParser("Usage: order_map_parallel.py -args [options]")
-	
-	parser.add_option("-p", "--pdb", action="store", dest="pdbFile")	
-	parser.add_option("-t", "--trj", action="store", dest="trjFile")
-	parser.add_option("-l", "--lipids", action="store", dest="lipidName")
-	parser.add_option("-m", "--leaf", action="store", dest="leafSide")
+	parser = argparse.ArgumentParser(description='Calculate the order parameter of each acyl-chain bond. \nOutput the time average order parameter in XY plane. \nThe \'trajectory stride\' and \'XY plane gridsize\' are hardcoded',
+						usage='use "%(prog)s --help" for more information', 
+						formatter_class=argparse.RawTextHelpFormatter)
 
-	parser.set_defaults(lipidName = "POPE POPG", leaflet = 1)
+	parser.add_argument("-p", "--pdbFile", required=True, help='coordinate file: .tpr, .pdb, .gro')
+	parser.add_argument("-t", "--trjFile", required=True, help='trajectory file: .dcd, .trr, .xtc')
+	parser.add_argument("-l", "--lipidName", help='lipid name: POPE POPG POPC POPS DLPE DLPG DLPC')
+	parser.add_argument("-m", "--leafSide", help='monolayer: 1 (up) 2 (down)')
 	
-	opts, args = parser.parse_args(cmdlineArgs)
-	pdbFile   = opts.pdbFile
-	trjFile   = opts.trjFile
-	lipidName = opts.lipidName
-	leafSide  = opts.leafSide
+	parser.set_defaults(lipidName = "POPC", leafSide = 1)
 	
-	# all filenames are required
-	if (pdbFile == None) or (trjFile == None):
+	args = parser.parse_args(cmdlineArgs)
 
+	if (args.pdbFile == None) or (args.trjFile == None):
+	
 		parser.print_help()
 		exit()
-	
-	return pdbFile, trjFile, lipidName, leafSide
+
+	return args.pdbFile, args.trjFile, args.lipidName, args.leafSide
 
 ##################################################################################################################################################################
 # order function
@@ -68,7 +77,7 @@ def parse_cmdline(cmdlineArgs):
 
 def Order(number_of_threads, thread_index):
 	"""
-	This function calculates the order parameter and the number of lipids on each grid bin.
+	This function calculates the cos(theta)^2 and the number of lipids on each grid bin.
 	It saves the results in scc.out and cnt.out files, two for each thread.
 	File should be combined 'a posteriori' to generate the order_parameter.map
 	"""
@@ -78,14 +87,32 @@ def Order(number_of_threads, thread_index):
 	# read trajectory with mdanalysis
 	
 	universe = mda.Universe(pdbFile, trjFile)
+
+	# set bead masses
+
+	#universe.atoms[universe.atoms.types == 'G'].masses = 1
+	#universe.atoms[universe.atoms.types == 'D'].masses = 1
+
+	# select lipid residues
 	
 	lipids = universe.select_atoms("resname {}".format(lipidName))
+
+	# save the box dimensions
+
 	(box_x,box_y) = (universe.dimensions[0],universe.dimensions[1])
 	
 	# save the bead pair for each bond in an array
 	
-	if lipidName == "POPE POPG": bond_names = "GL1-C1B GL2-C1A C1A-D2A D2A-C3A C3A-C4A C1B-C2B C2B-C3B C3B-C4B"
-	elif lipidName == "DLPE DLPG": bond_names = "GL1-C1A GL2-C1B C1A-C2A C2A-C3A C1B-C2B C2B-C3B"
+	PO = ['POPE', 'POPG', 'POPC', 'POPS']
+	DL = ['DLPE', 'DLPG', 'DLPC']
+
+	if all(x in PO for x in lipidName.split(" ")): 
+		bond_names = "GL1-C1A GL2-C1B C1A-D2A D2A-C3A C3A-C4A C1B-C2B C2B-C3B C3B-C4B"
+	elif all(x in DL for x in lipidName.split(" ")): 
+		bond_names = "GL1-C1A GL2-C1B C1A-C2A C2A-C3A C1B-C2B C2B-C3B"
+	else:
+		print "Error 'lipidName' NOT found in List : " , PO, DL
+		os._exit()
 	
 	enlace = []
 	for bond_name in bond_names.split():
@@ -122,7 +149,7 @@ def Order(number_of_threads, thread_index):
 	blockLength = int(len(universe.trajectory)/number_of_threads) # length of the trajectory fragments
 	start_frame = thread_index * blockLength # start on frame
 	end_frame = blockLength * (thread_index + 1) # end on frame
-	stride = 4 # analyze every 'stride' frames
+	stride = 1 # analyze every 'stride' frames
 	
 	# initialize array
 	
@@ -162,13 +189,47 @@ def Order(number_of_threads, thread_index):
 	# end traj cycle
 	
 	print "Saving results in thread {}".format(thread_index)
-	np.savetxt('tmp_{}/scc_{}_{}.out'.format(leafSide,lipidName[0:2],thread_index), order_array, fmt='%1.4e')
-	np.savetxt('tmp_{}/cnt_{}_{}.out'.format(leafSide,lipidName[0:2],thread_index), count_array, fmt='%1.4e')
+	np.savetxt('tmp_{}/scc_{}_{}.out'.format(leafSide,lipidName[0:2],thread_index), order_array, fmt='%1.4f')
+	np.savetxt('tmp_{}/cnt_{}_{}.out'.format(leafSide,lipidName[0:2],thread_index), count_array, fmt='%1.4f')
 	
 	print "Finished thread {}".format(thread_index)
 	
 	return 0
 
+##################################################################################################################################################################
+# calculate final map
+##################################################################################################################################################################
+
+def final(leafSide):
+	"""
+        This function calculates the final order parameter map
+        """
+	
+	path, dirs, files = next(os.walk('tmp_{}'.format(leafSide)))
+	file_count = len(files)
+	
+	A = 0
+	B = 0
+
+	print "Processing files:"
+
+	for name in files:
+		print name
+		if "cnt" in name:
+			a = np.loadtxt('{}/{}'.format(path,name))
+			A += a
+		elif "scc" in name:
+			b = np.loadtxt('{}/{}'.format(path,name))
+			B += b
+
+	np.savetxt('test.dat', B, fmt='%1.4f')
+
+	p1 = np.divide(A, B, where=B>=1)
+	p2 = 0.5 * (3.0 * p1 - 1.0)
+
+	np.savetxt('order_final_{}.dat'.format(leafSide), p2, fmt='%1.4f')
+	
+	return 0
 
 ##################################################################################################################################################################	
 # initialized each thread
@@ -208,7 +269,7 @@ def main():
 		print "Quitting normally"
 		pool.close()
 		pool.join()
-	
+
 	print 'main process exiting..'
 
 
@@ -231,7 +292,10 @@ if __name__ == "__main__":
 
 	# start the analysis
 	main()
-	
+
+	# process the results
+	final(leafSide)
+
 	# print the total time
 	print("--- %s seconds ---" % (time.time() - start_time))
 
